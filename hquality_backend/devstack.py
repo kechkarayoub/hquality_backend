3e157fe37c3701a49ee069e6fdaf327e99b621c3
PC <kechkarayoub@gmail.com>
2024-06-10 22:10:38 +0100

    deny dev keys to be ignored

--- hquality_backend/devstack.py
+++ hquality_backend/devstack.py
@@ -3,4 +3,4 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 # google storage config
-GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = join(dirname(abspath(__file__)), 'dev_mafatih/google_storage_configuration.json')  # path to private json key file obtained by Google.
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = join(dirname(abspath(__file__)), 'dev_keys/google_storage_configuration.json')  # path to private json key file obtained by Google.
